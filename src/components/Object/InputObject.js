f3f17a2366c996c38eb2a786d53e5e526e27f7d4
LSH7875 <tmdgml7875@gmail.com>
2021-09-09 19:14:38 +0900

    today object plus with create startbtn

--- src/components/Object/InputObject.js
+++ src/components/Object/InputObject.js
@@ -1,23 +1,42 @@
 import React ,{useState,useEffect} from 'react';
-import { View, Text,TextInput,Button} from 'react-native';
+import { View, Text,TextInput,Button,TouchableOpacity} from 'react-native';
+// import {closePopup} from '../../actions/popup'
+
 
 import {useCtx} from '../../Providers/AppProvider';
 const InputObect=()=>{
+
+
     const {dispatch,state} = useCtx();
     const [text, setText] = useState('');
+    
+    const inputObjectPlus=()=>{
+        dispatch({type:'ObjectPlus',payload:text})
+        setText("")
+    }
+
+
+    const closePopup=()=>{
+        dispatch({type:'PopupClose'})
+    }
     return(
         state.screen.popup===1?
-        <View style = {{flex:1,width:'50%',height:'50%',position:'absolute',zIndex:5,backgroundColor:'red',padding:'2%'}}>
-            <Text>목표를 입력하세요</Text>
+        <View style = {{flex:1,width:'50%',height:'50%',position:'absolute',borderRadius:10,zIndex:5,border:1,backgroundColor:'#ececec',padding:'2%'}}>
+            <Text style={{width:'100%', borderBottomColor:'skyblue',borderBottomWidth:1, textAlign:'center', padding:10}}>목표를 입력하세요</Text>
             <TextInput
-                style={{height: 40}}
-                placeholder="Type here to translate!"
+                style={{borderRadius:0,height: '80%',flexShrink:1,width:'80%',marginRight:'auto',marginLeft:'auto'}}
+                placeholder="오늘의 목표를 입력하세요"
                 onChangeText={text => setText(text)}
                 defaultValue={text}
+                multiline ={true}
             />
-            <View>
-                <Button title="입력"/>
-                <Button title="취소"/>
+            <View style={{flexDirection:'row'}}>
+                <TouchableOpacity style={{flex:1, marginRight:2,padding:5, backgroundColor:'skyblue',}} onPress={()=>inputObjectPlus()}>
+                    <Text style={{textAlign:"center"}}>입력</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{flex:1 ,marginLeft:2,padding:5,backgroundColor:'skyblue',}} onPress={()=>closePopup()}>
+                    <Text style={{textAlign:"center"}}>취소</Text>
+                </TouchableOpacity>
             </View>
         </View>
         :<></>
--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -2,7 +2,36 @@ const reducer = (state, action) => {
     switch (action.type) {
         case 'PlusObject':
             return {
-            ...state
+            ...state,screen:{...state.screen,popup:1}
+            }
+        case 'PopupClose':
+            return {
+                ...state,screen:{...state.screen,popup:0}
+                }
+        case 'ObjectPlus':
+            console.log('----------')
+            console.log(state.DailyObject)
+            console.log('----------')
+            
+            let dailyObject=(state.DailyObject===null)?[]:[...state.DailyObject] 
+
+            console.log(dailyObject)
+            if(dailyObject.length===0){
+                dailyObject.push({id:0,object:action.payload,priority:0,status:0,startDate:(new Date().toLocaleDateString()),endDate:(new Date().toLocaleDateString()),objStartTime:'오늘 시작시간으로',objEndTime:'오늘 끝나는 시간',Timelist:null,list:0})
+            }else{
+                console.log('dailyObject의 타입')
+                console.log(typeof dailyObject)
+                dailyObject.push({id:(state.DailyObject.length),object:action.payload,priority:0,status:0,startDate:(new Date().toLocaleDateString()),endDate:(new Date().toLocaleDateString()),objStartTime:'오늘 시작시간으로',objEndTime:'오늘 끝나는 시간',Timelist:null,list:0})
+            }
+                    
+            return {
+                ...state,DailyObject:dailyObject,screen:{...state.screen,popup:0}
+            }
+        case 'chooseObject':
+            console.log('오브젝트선택')
+            console.log(action.payload)
+            return{
+                ...state,screen:{...state.screen,objectProceeding:action.payload}
             }
         default:
             return state;
