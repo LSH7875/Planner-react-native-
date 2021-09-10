8f3b9d2c38af8d31858133e35c6c809737b2a29c
LSH7875 <tmdgml7875@gmail.com>
2021-09-10 19:11:37 +0900

    error fixed

--- src/utils/reducer.js
+++ src/utils/reducer.js
@@ -9,17 +9,14 @@ const reducer = (state, action) => {
                 ...state,screen:{...state.screen,popup:0}
                 }
         case 'ObjectPlus':
-            console.log('----------')
-            console.log(state.DailyObject)
-            console.log('----------')
             let f = new Date().toLocaleDateString()
             let e = new Date(f).getTime()/1000;
 
-            let dailyObject=(state.DailyObject===null)?{}:{...state.DailyObject} 
+            let dailyObject=(state.DailyObject===null)?{[`${f}`]:[]}:{...state.DailyObject} 
 
             console.log(dailyObject)
             if(dailyObject.f.length===0){
-                dailyObject.push({[`${f}`]:[{id:0,object:action.payload,priority:0,status:0,startDate:(new Date().toLocaleDateString()),endDate:(new Date().toLocaleDateString()),objStartTime:e,objEndTime:e,Timelist:null,list:0}]})
+                dailyObject.f.push({id:0,object:action.payload,priority:0,status:0,startDate:(new Date().toLocaleDateString()),endDate:(new Date().toLocaleDateString()),objStartTime:e,objEndTime:e,Timelist:null,list:0})
             }else{
                 dailyObject.f.push({id:(state.DailyObject.f.length),object:action.payload,priority:0,status:0,startDate:(new Date().toLocaleDateString()),endDate:(new Date().toLocaleDateString()),objStartTime:e,objEndTime:e,Timelist:null,list:0})
             }
