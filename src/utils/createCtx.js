645c681c4758e44c88d8963d5d0370d79d53a3a8
LSH7875 <tmdgml7875@gmail.com>
2021-09-25 01:21:57 +0900

    stop & pause

--- src/utils/createCtx.js
+++ src/utils/createCtx.js
@@ -1,7 +1,7 @@
 // Context 생성하기
 import React from 'react';
 import {usePersistedContext} from '../utils/usePersistant'
-
+import {today} from '../components/Hooks/time'
 export const initialState = {
   DailyObject:null,//{id/object/priority/satus/startDate/endDate/objStartTime/objEndTime/Timelist/list}
   Schedule:null,//{id/schedule/start/end/list/alram}
@@ -11,7 +11,7 @@ export const initialState = {
   Memo:null,//{id/subject/content/index/}
   Project:null,//{id/project/start_date/end_date/status/satisfaction/list/specific/list/memo}
   //sepcific:{specificObj/priority/startDate/endDate/status/memo}
-  screen:{popup:0,objectProceeding:null,Proceeding:0},//{popup/objectProceeding/time}
+  screen:{popup:0,objectProceeding:null,Proceeding:0,day:today},//{popup/objectProceeding/time}
   //objectProceeding: 선택하는 목표 하나만 가능하게끔 하는 항목
   //Proceeding:0,1//0이면 아무것도 실행을 안하는 거니까  움직임 가능하게
   // 1이면 실행을 하는 중이니까 움직이지 못하게 해야함.
@@ -23,7 +23,7 @@ const createCtx = () => {
   const ctx = React.createContext(initialState);
   const useCtx = (Ctx=ctx) => {
     const c = (React.useContext(Ctx));
-    //const c = usePersistedContext(React.useContext(Ctx),"plannerwing");
+    // const c = usePersistedContext(React.useContext(Ctx));
     return c;
   };
 
--- src/utils/usePersistant.js
+++ src/utils/usePersistant.js
@@ -1,14 +1,38 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import symbolicateStackTrace from 'react-native/Libraries/Core/Devtools/symbolicateStackTrace';
 
-export const usePersistedContext = async(context,key = "@plannerwing")=>{
+const key = "plannerwing";
+
+// useEffect=async(context)=>{
+//     try{
+//         let plan = await AsyncStorage.getItem(key);
+//         if(plan){
+//             plan=JSON.parse(plan);
+//             useState({plan});
+//         }
+//     } catch(e){
+//         console.log('error from AsyncStorage : ',e)
+//     }
+// }
+
+// addPlan = (plan)=>{
+//     const plan = state;
+//     symbolicateStackTrace({plan})
+//     AsyncStorage.setItem(key,JSON.stringify(plan))
+//         .then(()=>{console.log('storage updated!')})
+//         .catch((e)=>{console.log('addplan error: ',e)})
+// }
+export const usePersistedContext = async(context)=>{
+    //읽어오기임.
     const persistedContext = await AsyncStorage.getItem(key);
     return persistedContext ? JSON.parse(presistedContext):context;
 }
 
 
 
-export const usePersistedReducer = ([state,dispatch],key = "@plannerwing")=>{
+export const usePersistedReducer = ({state,dispatch})=>{
+    //아이템 설정하는 것...
     useEffect(()=>AsyncStorage.setItem(key,JSON.stringify(state)),[state]);
     return [state,dispatch]
 }
